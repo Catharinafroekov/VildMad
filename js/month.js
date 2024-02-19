e61c96092f629b346ecc74982f8cd1a907a79ceb
Catharina <catharina2002@gmail.com>
2024-02-19 15:31:46 +0100

    month og filtrering

--- js/month.js
+++ js/month.js
@@ -1,7 +1,12 @@
 //https://lumyjlzobfetmedfjjvk.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1bXlqbHpvYmZldG1lZGZqanZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDQxMzYsImV4cCI6MjAyMzQ4MDEzNn0.ZXiU7EEPPrOfTnQRn4VAzqCBf3ULvmz5kQJ0vfTjFZg
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const myCategory = urlParams.get("category"); 
+document.querySelector("#monthName h1").textContent = myCategory;
 
-fetch("https://lumyjlzobfetmedfjjvk.supabase.co/rest/v1/VildMad", {
+
+fetch(`https://lumyjlzobfetmedfjjvk.supabase.co/rest/v1/VildMad?month=eq.${myCategory}`, {
   method: "GET",
   headers: {
     apikey:
@@ -13,12 +18,6 @@ fetch("https://lumyjlzobfetmedfjjvk.supabase.co/rest/v1/VildMad", {
 
 function showData(categories) {
 const monthNameSection = document.getElementById("monthName");
-
-// Assuming the first category contains the month name, adjust as needed
-if (categories.length > 0) {
-  const monthName = categories[0].month;
-  monthNameSection.innerHTML = `<h1>${monthName}</h1>`;
-}
 
 categories.forEach(showCategories);
 
